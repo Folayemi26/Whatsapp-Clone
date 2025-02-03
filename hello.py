a02c41d607595f103a8a232345a0599b8c79a17e
Folayemi26 <adebayozeba@gmail.com>
2025-02-02 23:22:59 -0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,34 @@
+
+# a program to add two numbers
+'''a= int(input("Enter your first number: "))
+b= int (input("Enter your second number: "))
+c= int(input("Enter your third number: "))
+division = a / b / ca
+print( division ) '''
+
+'''a= input("Enter your first name: ") 
+b= input("Enter your last name: ")
+c= input ("Enter your room number: ")
+d= input("Enter your Phone number: ")
+e= input("Enter your matric number: ")
+f= input("Enter your program: ")
+g= input("Enter your department: ")
+h= input("Enter your age: ")
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)'''
+
+'''a= input("Enter your first name: ") 
+b= input("Enter your last name: ")
+c= input ("Enter your room number: ")
+d= input("Enter your Phone number: ")
+e= input("Enter your matric number: ")
+f= input("Enter your program: ")
+g= input("Enter your department: ")
+h= input("Enter your age: ")
+print(a+b+c+d+e+f+g+h)'''
+
+"a= input("Enter your ")
